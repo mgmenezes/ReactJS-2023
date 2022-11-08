b7e513309285fc96b6a4ba09ffaa89cda432af3a
Moises Menezes <moises.menezes@liferay.com>
2022-11-08 00:58:00 -0300

    Starting new Project Reactjs+Vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+import "./styles.css";
+export function App() {
+  return (
+    <div>
+      <Header />
+      <Post
+        author="moises"
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid debitis odit, similique error earum quaerat mollitia provident accusamus,culpa laudantium molestiae sed cum perferendis est facere ullam, assumenda optio vel?"
+      />
+      <Post author="Gabriel" content="Post massa" />
+    </div>
+  );
+}
+export default App;
